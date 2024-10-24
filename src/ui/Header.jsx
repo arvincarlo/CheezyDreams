52ccc233405a1a6c60caa6961a8191d8bbfd24b1
Arvin <arvincris@gmail.com>
2024-10-24 11:52:29 +0800

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Cheezy Dreams Co.</Link>
+            <p>Arvin</p>
+        </header>
+    )
+}
+
+export default Header
