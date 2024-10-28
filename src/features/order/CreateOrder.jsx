c04cd4599875cf1c710a569449e2126aa38b39cd
Arvin <arvincris@gmail.com>
2024-10-28 11:05:30 +0800

    Error Handling in Form Actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 
 import { createOrder } from "../../services/apiRestaurant";
 
@@ -34,6 +33,12 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
+  console.log(formErrors);
+  
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -52,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +80,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name='cart' value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Placing Order...' : 'Order now'}</button>
         </div>
       </Form>
     </div>
@@ -84,6 +90,7 @@ function CreateOrder() {
 export default CreateOrder;
 
 export async function action({request}) {
+  // Define the order object
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
@@ -92,9 +99,17 @@ export async function action({request}) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on"
   }
-  
+
+  // Error Handing
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Please give us your correct phone number. We might need to contact you.";
+  }
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // Creating the data and redirect
   const newOrder = await createOrder(order);
-  console.log(newOrder);
-  
+
   return redirect(`/order/${newOrder.id}`);
 }
