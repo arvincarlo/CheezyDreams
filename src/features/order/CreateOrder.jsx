d311161306e0bb5eca6da89b1e9d637fae60de59
Arvin <arvincris@gmail.com>
2024-10-29 15:09:56 +0800

    Reusing Styles with @apply / Reusing Styles with React Components

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -48,13 +49,13 @@ function CreateOrder() {
             <Form method="POST">
                 <div>
                     <label>First Name</label>
-                    <input type="text" name="customer" required />
+                    <input className="input" type="text" name="customer" required />
                 </div>
 
                 <div>
                     <label>Phone number</label>
                     <div>
-                        <input type="tel" name="phone" required />
+                        <input className="input" type="tel" name="phone" required />
                     </div>
                     {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
@@ -63,7 +64,7 @@ function CreateOrder() {
                     <label>Address</label>
                     <div>
                         <input
-                            className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+                            className="input"
                             type="text"
                             name="address"
                             required
@@ -91,12 +92,9 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button
-                        className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-                        disabled={isSubmitting}
-                    >
+                    <Button disabled={isSubmitting}>
                         {isSubmitting ? "Placing Order..." : "Order now"}
-                    </button>
+                    </Button>
                 </div>
             </Form>
         </div>
