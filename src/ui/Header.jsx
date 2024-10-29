72a994df1190e5bfc19120c048c32cc03ae89c4c
Arvin <arvincris@gmail.com>
2024-10-29 10:23:13 +0800

    Responsive Design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:p-6">
       <Link to="/" className="tracking-widest">
         Cheezy Dreams Co.
       </Link>
