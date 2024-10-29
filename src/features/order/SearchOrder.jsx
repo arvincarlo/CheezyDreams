f4722fe5189fad7e521d3edbcdc216b19df3ddf0
Arvin <arvincris@gmail.com>
2024-10-29 11:57:16 +0800

    Styling Form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState("");
     const navigate = useNavigate();
 
     function handleSubmit(e) {
@@ -11,14 +11,19 @@ function SearchOrder() {
 
         if (!query) return;
         navigate(`/order/${query}`);
-        setQuery('');
+        setQuery("");
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input placeholder="Search order #" value={query} onChange={e => setQuery(e.target.value)}></input>
+            <input
+                className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-800 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+                placeholder="Search order #"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            ></input>
         </form>
-    )
+    );
 }
 
-export default SearchOrder
+export default SearchOrder;
