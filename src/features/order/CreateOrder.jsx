3187939f3f11e6c9c1ea43acb791c93fe719215c
Arvin <arvincris@gmail.com>
2024-10-30 11:40:49 +0800

    Enable the Create Order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -140,8 +140,7 @@ export async function action({ request }) {
     if (Object.keys(errors).length > 0) return errors;
 
     // Creating the data and redirect
-    // const newOrder = await createOrder(order);
+    const newOrder = await createOrder(order);
 
-    // return redirect(`/order/${newOrder.id}`);
-    return null;
+    return redirect(`/order/${newOrder.id}`);
 }
