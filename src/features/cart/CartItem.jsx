66b650ef8f4fa65e62ab69bb351e9fea0a0898de
Arvin <arvincris@gmail.com>
2024-10-30 16:57:43 +0800

    Code Cleanup for Store

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,13 @@
+import { useDispatch } from "react-redux";
+
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { deleteItem } from "./cartSlice";
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { pizzaId, name, quantity, totalPrice, } = item;
+  const dispatch = useDispatch();
+  console.log(item);
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,6 +17,7 @@ function CartItem({ item }) {
       <div className="flex justify-between items-center sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
+        <button onClick={() => dispatch(deleteItem(pizzaId))}>Remove {pizzaId}</button>
       </div>
     </li>
   );
