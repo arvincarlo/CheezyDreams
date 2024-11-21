909efd35b34913b6a4554e3c2281d53d624cbef3
Arvin Cris <arvincris97@gmail.com>
2024-11-21 19:32:19 +0800

    Updated button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,7 @@ function Button({ children, disabled = false, to, type, onClick }) {
         primary: base + " px-4 py-3 sm:px-6 sm:py-4 bg-yellow-400 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300",
         small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs bg-yellow-400 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300",
         round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm bg-yellow-400 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300",
-        warning: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs bg-orange-400 hover:bg-orange-300 focus:bg-orange-300 focus:outline-none focus:ring focus:ring-orange-300",
+        warning: base + " px-4 py-2 md:px-5 md:py-2.5 sm:text-[12px] text-xs bg-orange-400 hover:bg-orange-300 focus:bg-orange-300 focus:outline-none focus:ring focus:ring-orange-300",
         secondary:
             "inline-block rounded-full text-sm border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:text-stone-800 disabled:cursor-not-allowed px-4 py-2.5 md:px-5 md:py-3.5",
     };
